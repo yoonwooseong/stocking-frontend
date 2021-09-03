c16005dc12a0d9fe01041504535f99d1b8e40f69
Wooseong <57824259+yoonwooseong@users.noreply.github.com>
2021-09-03 14:41:37 +0900

    j-t-mcc를 이용한 chart 그리기

---
+++ src/components/PieChart.vue
@@ -0,0 +1,34 @@
+<template>
+  <div style="height:300px;width:300px;">
+    <vue3-chart-js v-bind="{ ...pieChart }" />
+  </div>
+</template>
+
+<script>
+import Vue3ChartJs from "@j-t-mcc/vue3-chartjs";
+
+export default {
+  name: "PieChart",
+  components: {
+    Vue3ChartJs,
+  },
+  setup() {
+    const pieChart = {
+      type: "pie",
+      data: {
+        labels: ["수입", "지출", "저축"],
+        datasets: [
+          {
+            backgroundColor: ["#41B883", "#E46651", "#5586EB"],
+            data: [80, 39, 41],
+          },
+        ],
+      },
+    };
+
+    return {
+      pieChart,
+    };
+  },
+};
+</script>
