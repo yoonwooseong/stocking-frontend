8da3fc7b8b6a2068cb6b98ca3a3bcb766d87c853
Wooseong <57824259+yoonwooseong@users.noreply.github.com>
2021-09-03 14:41:50 +0900

    v-model 테스트

--- src/views/Main.vue
+++ src/views/Main.vue
@@ -8,6 +8,8 @@
       <router-link to="/asset" class="routerAssetBtn" >내 자산</router-link>
     </div>
   </div>
+  <input v-model="message" placeholder="여기를 수정해보세요">
+    <p>메시지: {{ message }}</p>
 </template>
 
 <script>
